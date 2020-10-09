89fc6c566342818f7573c7e9888de68d383733d7
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2020-10-09 12:46:05 +0700

    Complete task 2.1

---
+++ Task_2_1/src/main/java/com/kkmoskalenko/oop/Stack.java
@@ -0,0 +1,50 @@
+package com.kkmoskalenko.oop;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Stack<T> implements Iterable<T> {
+    @SuppressWarnings("unchecked")
+    private T[] stack = (T[]) new Object[0];
+
+    private int count = 0;
+    private int capacity = 0;
+
+    public void push(T item) {
+        if (count == capacity) {
+            capacity = capacity * 2 + 1;
+            stack = Arrays.copyOf(stack, capacity);
+        }
+
+        stack[count++] = item;
+    }
+
+    public T pop() {
+        if (count == 0) {
+            return null;
+        }
+
+        return stack[--count];
+    }
+
+    public int count() {
+        return count;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < count;
+            }
+
+            @Override
+            public T next() {
+                return stack[index++];
+            }
+        };
+    }
+}
---
+++ Task_2_1/src/test/java/com/kkmoskalenko/oop/StackTest.java
@@ -0,0 +1,73 @@
+package com.kkmoskalenko.oop;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class StackTest extends TestCase {
+
+    public void testIntStack() {
+        Stack<Integer> stack = new Stack<>();
+        int elem;
+
+        stack.push(123);
+        stack.push(456);
+
+        assertEquals(2, stack.count());
+
+        elem = stack.pop();
+        assertEquals(456, elem);
+        assertEquals(1, stack.count());
+
+        stack.push(789);
+        assertEquals(2, stack.count());
+
+        elem = stack.pop();
+        assertEquals(789, elem);
+
+        elem = stack.pop();
+        assertEquals(123, elem);
+
+        assertEquals(0, stack.count());
+
+        stack.pop();
+        assertEquals(0, stack.count());
+    }
+
+    public void testStringStack() {
+        Stack<String> stack = new Stack<>();
+        String elem;
+
+        stack.push("hello");
+        stack.push("world");
+
+        assertEquals(2, stack.count());
+
+        elem = stack.pop();
+        assertEquals("world", elem);
+        assertEquals(1, stack.count());
+
+        elem = stack.pop();
+        assertEquals("hello", elem);
+        assertEquals(0, stack.count());
+    }
+
+    public void testIterator() {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(9);
+        stack.push(2);
+        stack.push(1);
+        stack.push(3);
+
+        int[] array = new int[5];
+        int[] res = {1, 9, 2, 1, 3};
+
+        int i = 0;
+        for (int num : stack) {
+            array[i++] = num;
+        }
+
+        Assert.assertArrayEquals(res, array);
+    }
+}
