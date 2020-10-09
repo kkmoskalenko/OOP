c8f0d16d292a3a8cbb0bce9c13b072a4cb0348b8
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2020-10-09 12:55:54 +0700

    Add the try-with-resources statement

--- Task_1_2/src/main/java/com/kkmoskalenko/oop/Substring.java
+++ Task_1_2/src/main/java/com/kkmoskalenko/oop/Substring.java
@@ -7,33 +7,33 @@ import java.util.List;
 
 public class Substring {
     public static int[] findIndices(String pathname, String substring) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
                 new FileInputStream(pathname), StandardCharsets.UTF_8
-        ));
+        ))) {
+            int currentPos = 0;
+            int bufferLength = substring.length();
 
-        int currentPos = 0;
-        int bufferLength = substring.length();
+            char[] buffer = new char[bufferLength];
+            char[] prevBuffer = new char[bufferLength];
 
-        char[] buffer = new char[bufferLength];
-        char[] prevBuffer = new char[bufferLength];
+            List<Integer> indices = new ArrayList<>();
 
-        List<Integer> indices = new ArrayList<>();
+            while (reader.read(buffer) != -1) {
+                String str = new String(prevBuffer) + new String(buffer);
+                int index = str.indexOf(substring);
+                if (index != -1) {
+                    index = index - bufferLength + currentPos;
 
-        while (reader.read(buffer) != -1) {
-            String str = new String(prevBuffer) + new String(buffer);
-            int index = str.indexOf(substring);
-            if (index != -1) {
-                index = index - bufferLength + currentPos;
-
-                if (indices.isEmpty() || index != indices.get(indices.size() - 1)) {
-                    indices.add(index);
+                    if (indices.isEmpty() || index != indices.get(indices.size() - 1)) {
+                        indices.add(index);
+                    }
                 }
+
+                currentPos += bufferLength;
+                prevBuffer = buffer.clone();
             }
 
-            currentPos += bufferLength;
-            prevBuffer = buffer.clone();
+            return indices.stream().mapToInt(i -> i).toArray();
         }
-
-        return indices.stream().mapToInt(i -> i).toArray();
     }
 }
