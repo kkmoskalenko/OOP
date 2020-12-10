4db867cce9e37555960cebbe2cacba4c1418f113
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2020-12-10 23:51:39 +0700

    Add more complicated tests

--- Task_4_1/src/test/java/com/kkmoskalenko/oop/CalculatorTest.java
+++ Task_4_1/src/test/java/com/kkmoskalenko/oop/CalculatorTest.java
@@ -11,5 +11,10 @@ public class CalculatorTest extends TestCase {
         assertEquals(0.0, Calculator.evaluate("sin + - 1 2 1"));
         assertEquals(25.0, Calculator.evaluate("pow + 3 2 2"));
         assertEquals(5.0, Calculator.evaluate("sqrt * 5 + 1 4"));
+
+        assertEquals(41607.731092436974, Calculator
+                .evaluate("+ 98 * - * 22 31 * / 5 + 82 37 36 61"));
+        assertEquals(-19529.652173913048, Calculator
+                .evaluate("- * 29 - * 44 / + 75 93 23 * 62 16 82"));
     }
 }
