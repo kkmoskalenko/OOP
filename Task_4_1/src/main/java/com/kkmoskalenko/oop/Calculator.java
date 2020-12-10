53a84109c78874bd5ec2d7d00165a070ffd0fc9b
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2020-12-10 23:31:44 +0700

    Replace Stack with Deque

--- Task_4_1/src/main/java/com/kkmoskalenko/oop/Calculator.java
+++ Task_4_1/src/main/java/com/kkmoskalenko/oop/Calculator.java
@@ -1,35 +1,36 @@
 package com.kkmoskalenko.oop;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Calculator {
     public static double evaluate(String expression) {
-        Stack<Double> stack = new Stack<>();
+        Deque<Double> deque = new ArrayDeque<>();
         String[] tokens = expression.split(" ");
 
         for (int i = tokens.length - 1; i >= 0; i--) {
             try {
-                stack.push(Double.parseDouble(tokens[i]));
+                deque.push(Double.parseDouble(tokens[i]));
             } catch (NumberFormatException e) {
                 switch (tokens[i]) {
-                    case "+" -> stack.push(stack.pop() + stack.pop());
-                    case "-" -> stack.push(stack.pop() - stack.pop());
-                    case "*" -> stack.push(stack.pop() * stack.pop());
-                    case "/" -> stack.push(stack.pop() / stack.pop());
+                    case "+" -> deque.push(deque.pop() + deque.pop());
+                    case "-" -> deque.push(deque.pop() - deque.pop());
+                    case "*" -> deque.push(deque.pop() * deque.pop());
+                    case "/" -> deque.push(deque.pop() / deque.pop());
 
-                    case "log" -> stack.push(Math.log(stack.pop()));
-                    case "pow" -> stack.push(Math.pow(stack.pop(), stack.pop()));
-                    case "sqrt" -> stack.push(Math.sqrt(stack.pop()));
-                    case "sin" -> stack.push(Math.sin(stack.pop()));
-                    case "cos" -> stack.push(Math.cos(stack.pop()));
+                    case "log" -> deque.push(Math.log(deque.pop()));
+                    case "pow" -> deque.push(Math.pow(deque.pop(), deque.pop()));
+                    case "sqrt" -> deque.push(Math.sqrt(deque.pop()));
+                    case "sin" -> deque.push(Math.sin(deque.pop()));
+                    case "cos" -> deque.push(Math.cos(deque.pop()));
 
                     default -> throw new RuntimeException("Invalid operator: " + tokens[i]);
                 }
             }
         }
 
-        return stack.peek();
+        return deque.peek();
     }
 
     public static void main(String[] args) {
